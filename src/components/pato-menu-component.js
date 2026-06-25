e5830d71eb4d196239edc5f77573db7826749879
PATOLINODD <psodesenhos@gmail.com>
2026-06-24 23:19:16 -0300

    update -> changed the menu component and animation.

--- src/components/pato-menu-component.js
+++ src/components/pato-menu-component.js
@@ -5,32 +5,53 @@ class PatoMenuComponent extends HTMLElement {
         this.attachShadow({ mode: 'open' })
         this.shadowRoot.innerHTML = `
             <style>
-                :host {
+                :host, 
+                .menu {
                     display: flex;
                     flex-direction: column;
                     justify-content: space-between;
                     align-items: center;
-                    height: 100%;
-                    width: 3rem;
                     padding: .25rem 0;
+                    cursor: pointer;
+                    gap: .25rem;
                 }
 
-                div {
-                    width: 100%;
+                .top,
+                .mid,
+                .bottom {
+                    width: 3rem;
                     height: .25rem;
                     background-color: var(--secondary-color, hsl(0, 0%, 70%));
                     will-change: transform, opacity;
-                    transform-origin: left;
+                    transform-origin: center;
+                    transition: all 333ms ease-out;
+                }
+
+                .mid {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                }
+
+                .x-animation {
+                    position: relative;
+                    width: 100%;
+                    height: .25rem;
                 }
 
             </style>
-            <div class="top"></div>
-            <div class="mid"></div>
-            <div class="bottom"></div>
+            <div class="menu">
+                <div class="top"></div>
+                <div class="x-animation">
+                    <div class="mid"></div>
+                    <div class="mid"></div>
+                </div>
+                <div class="bottom"></div>
+            </div>
         `;
 
         this.top = this.shadowRoot.querySelector('div.top');
-        this.mid = this.shadowRoot.querySelector('div.mid');
+        this.mid = this.shadowRoot.querySelectorAll('div.mid');
         this.bottom = this.shadowRoot.querySelector('div.bottom');
 
         this.isOpened = false;
@@ -41,32 +62,34 @@ class PatoMenuComponent extends HTMLElement {
     }
 
     transform() {
-        this.top.animate(
+        this.isOpened = !this.isOpened;
+        const menu = this.shadowRoot.querySelector(".menu");
+
+        const menuAnimation = menu.animate(
             {
-                transform: ['translateX(.4rem) rotate(45deg)'],
+                transform: ['rotate(90deg)']
             },
             {
-                duration: 333, fill: 'forwards', easing: 'ease-out'
+                duration: 333, fill: 'forwards', easing: 'ease-out', composite: 'accumulate'
             }
         );
 
-        this.mid.animate(
-            {
-                opacity: [0]
-            },
-            {
-                duration: 333, fill: 'forwards', easing: 'ease-out'
-            }
-        );
+        menuAnimation.onfinish = () => {
+            menuAnimation.commitStyles();
+            menuAnimation.cancel();
+        }
 
-        this.bottom.animate(
-            {
-                transform: ['translateX(.4rem) rotate(-45deg)'],
-            },
-            {
-                duration: 333, fill: 'forwards', easing: 'ease-out'
-            }
-        );
+        if (this.isOpened) {
+            this.top.style.opacity = 0;
+            this.bottom.style.opacity = 0;
+            this.mid[0].style.transform = 'rotate(45deg)';
+            this.mid[1].style.transform = 'rotate(-45deg)';
+        } else {
+            this.top.style.opacity = 1;
+            this.bottom.style.opacity = 1;
+            this.mid[0].style.transform = 'rotate(0)';
+            this.mid[1].style.transform = 'rotate(0)';
+        }
     }
 }
 
